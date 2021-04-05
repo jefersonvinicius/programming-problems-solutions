6f0f599edb32461771eafb198192a7d701422394
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-04-05 09:04:26 -0300

    Add golang utilities reader and reader codes

--- utilities/golang.go
+++ utilities/golang.go
@@ -4,10 +4,39 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var reader *bufio.Reader = bufio.NewReader(os.Stdin)
 var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
 
 func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
-func scanf(f string, a ...interface{}) { fmt.Fscanf(reader, f, a...) }
+func scanf(f string, a ...interface{})  { fmt.Fscanf(reader, f, a...) }
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	readLine(reader)
+
+	// Resolve problem
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) (string, error) {
+	str, _, err := reader.ReadLine()
+	return strings.TrimRight(string(str), "\r\n"), err
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
