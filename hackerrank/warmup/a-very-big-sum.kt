90301e148c946254cf99c8163504cf95442d75f1
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-11-12 23:30:40 -0300

    Add HR A very big sum problem solution

---
+++ hackerrank/warmup/a-very-big-sum.kt
@@ -0,0 +1,38 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+
+fun aVeryBigSum(array: Array<Long>): Long {
+    var result: Long = 0
+    for (item in array) {
+        result += item
+    }
+    return result
+
+}
+
+fun main(args: Array<String>) {
+    val arCount = readLine()!!.trim().toInt()
+
+    val ar = readLine()!!.trimEnd().split(" ").map{ it.toLong() }.toTypedArray()
+
+    val result = aVeryBigSum(ar)
+
+    println(result)
+}
