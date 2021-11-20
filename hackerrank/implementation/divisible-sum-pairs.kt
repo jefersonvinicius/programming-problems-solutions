841cd6be2baa5ee8e4bb0a58919cd00e6706bc09
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-11-19 23:51:27 -0300

    Start resolve HR Divisible Sum Pair problem

--- hackerrank/implementation/divisible-sum-pairs.kt
+++ hackerrank/implementation/divisible-sum-pairs.kt
@@ -17,17 +17,22 @@ import kotlin.ranges.*
 import kotlin.sequences.*
 import kotlin.text.*
 
-fun kangaroo(x1: Int, v1: Int, x2: Int, v2: Int): String {
-    if (v1 < v2 || ) return "NO"
-    return "YES"
+
+fun divisibleSumPairs(n: Int, k: Int, ar: Array<Int>): Int {
+    // Write your code here
+
 }
 
 fun main(args: Array<String>) {
     val first_multiple_input = readLine()!!.trimEnd().split(" ")
-    val x1 = first_multiple_input[0].toInt()
-    val v1 = first_multiple_input[1].toInt()
-    val x2 = first_multiple_input[2].toInt()
-    val v2 = first_multiple_input[3].toInt()
-    val result = kangaroo(x1, v1, x2, v2)
+
+    val n = first_multiple_input[0].toInt()
+
+    val k = first_multiple_input[1].toInt()
+
+    val ar = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = divisibleSumPairs(n, k, ar)
+
     println(result)
 }
