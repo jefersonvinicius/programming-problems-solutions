d85778c3dc6d58c2cffbc9fb516b363c6a761f79
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-11-25 00:07:21 -0300

    Resolved HR sock merchant problem solution

--- hackerrank/implementation/sock-merchant.kt
+++ hackerrank/implementation/sock-merchant.kt
@@ -1,1 +1,39 @@
-// Start
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+fun sockMerchant(n: Int, socks: Array<Int>): Int {
+    val socksAmount = mutableMapOf<Int, Int>()
+    for (sock in socks) {
+        if (!socksAmount.containsKey(sock)) socksAmount[sock] = 0
+        socksAmount[sock] = socksAmount.getValue(sock) + 1
+    }
+
+    val result = socksAmount.values.fold(0) {
+        total, current -> total + Math.floor(current / 2.0).toInt()
+    }
+
+    return result
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+    val ar = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    val result = sockMerchant(n, ar)
+    println(result)
+}
