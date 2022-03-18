2da9de63c157d3e4b46ef6bbd9f037a8f639b6ce
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-03-18 18:39:01 -0300

    Trying the 1396 problem again

--- urionlinejudge/adhoc/1396.cpp
+++ urionlinejudge/adhoc/1396.cpp
@@ -14,6 +14,8 @@ vector<string> lesser_lexicographic_list(vector<string> list1, vector<string> li
 
 vector<string> sorting(vector<string> names, int max_swaps, int initial_index) {
     vector<string> result = names;
+    if (result.size() == 1) return result;
+
     int swaps = 0;
     int index = initial_index;
     while (swaps < max_swaps) {
@@ -36,34 +38,36 @@ vector<string> sorting(vector<string> names, int max_swaps, int initial_index) {
     return result;
 }
 
+void print_vector(vector<string> v) {
+    for (auto item : v) {
+        printf("%s ", item.c_str());
+    }
+}
+
 int main() {
 
     int n, k, instance = 1;
     while (1) {
-        scanf("%d %d", &n, &k);
+        fflush(stdin);
+        scanf("%d%d\n", &n, &k);
         if (n == 0 && k == 0) break;
 
         vector<string> names;
         char name_c[21];
         for (int i = 0; i < n; i++) {
-            scanf("%s", name_c);
+            scanf("%s \n", name_c);
             names.push_back(name_c);
         }
 
         vector<vector<string>> lists;
-        vector<string> answer;
+        vector<string> answer = names;
         for (int i = 0; i < names.size() - 1; i++) {
             vector<string> sorted = sorting(names, k, i);
             lists.push_back(sorted);
-            if (answer.empty()) {
-                answer = sorted;
-            } else {
-                answer = lesser_lexicographic_list(answer, sorted);
-            }
+            answer = lesser_lexicographic_list(answer, sorted);
         }
 
         printf("Instancia %d\n", instance);
-
         for (int i = 0; i < answer.size(); i++) {
             printf("%s ", answer[i].c_str());
         }
