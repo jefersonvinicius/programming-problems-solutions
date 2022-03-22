b2881f399c83a0e174b56bf3d24d5e4ea3224f76
Jeferson Vinicius <jeferson.viniciuscrc@gmail.com>
2022-03-21 23:18:34 -0300

    Still trying the 1396 problem

--- urionlinejudge/adhoc/1396.cpp
+++ urionlinejudge/adhoc/1396.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,29 +13,31 @@ vector<string> lesser_lexicographic_list(vector<string> list1, vector<string> li
     return list1;
 }
 
-vector<string> sorting(vector<string> names, int max_swaps, int initial_index) {
+int index_of(vector<string> items, string target) {
+    vector<string>::iterator it = find(items.begin(), items.end(), target);
+    if (it != items.end()) return it - items.begin();
+    return -1;
+}
+
+vector<string> sorting(vector<string> names, int max_swaps, string target) {
     vector<string> result = names;
     if (result.size() == 1) return result;
 
     int swaps = 0;
-    int index = initial_index;
-    while (swaps < max_swaps) {
-        bool some_swap_done = false;
-        for (int i = index; i < result.size() - 1; i++) {
-            if (result[i] > result[i+1]) {
-                some_swap_done = true;
-                string tmp = result[i];
-                result[i] = result[i+1];
-                result[i+1] = tmp; 
-                
-                swaps++;
-                if (swaps == max_swaps) break;
-            }
+    int index = index_of(names, target);
+    while (swaps < max_swaps && index < names.size() - 1) {
+        while (result[index] > result[index + 1]) {
+            string tmp = result[index];
+            result[index] = result[index + 1];
+            result[index + 1] = tmp;
+            swaps++;
+            index--;
+            if (swaps == max_swaps || index < 0) break;
         }
-
-        if (!some_swap_done && index == 0) break;
-        index = 0;
+        if (index < 0) break;
+        index++;
     }
+
     return result;
 }
 
@@ -48,7 +51,6 @@ int main() {
 
     int n, k, instance = 1;
     while (1) {
-        fflush(stdin);
         scanf("%d%d\n", &n, &k);
         if (n == 0 && k == 0) break;
 
@@ -61,8 +63,14 @@ int main() {
 
         vector<vector<string>> lists;
         vector<string> answer = names;
-        for (int i = 0; i < names.size() - 1; i++) {
-            vector<string> sorted = sorting(names, k, i);
+        vector<string> sorted_names = names;
+        sort(sorted_names.begin(), sorted_names.end());
+        // printf("Sorted: ");
+        // print_vector(sorted_names);
+        // printf("\n");
+
+        for (int i = 0; i < sorted_names.size(); i++) {
+            vector<string> sorted = sorting(names, k, sorted_names[i]);
             lists.push_back(sorted);
             answer = lesser_lexicographic_list(answer, sorted);
         }
