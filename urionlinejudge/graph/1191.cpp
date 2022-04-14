94f3231a9ac5885500a8712037be56e712edb258
Jeferson Vinicius <jeferson.viniciuscrc@gmail.com>
2022-04-14 08:24:23 -0300

    Add 1191 URI problem solution

---
+++ urionlinejudge/graph/1191.cpp
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <string>
+
+#define MAX_TEXT 27
+
+using namespace std;
+
+int indexOf(string str, int left, int right, char target) {
+    int result = -1;
+    for (int i = left; i <= right; i++) {
+        if (str[i] == target) {
+            result = i;
+            break;
+        }
+    }
+    return result;
+}
+
+class Node {
+    public:
+        char value;
+        Node* left;
+        Node* right;
+
+        Node() {}
+
+        Node(char value) {
+            this->value = value;
+            this->right = NULL;
+            this->left = NULL;
+        }
+};
+
+class Tree {
+    public:
+        Node* root = NULL;
+
+        Tree() {}
+
+        string postOrder() {
+            string result = "";
+            this->postOrderRecursion(this->root, &result);
+            return result;
+        }
+
+        static Tree buildFromPreAndInOrder(string preOrder, string inOrder) {
+            int preOrderIndex = 0;
+            Node *root = new Node(preOrder[preOrderIndex]);
+            int inIndex = indexOf(inOrder, 0, inOrder.size() - 1, root->value);
+
+            Node* left = Tree::buildNode(preOrder, inOrder, 0, inIndex - 1, &preOrderIndex);
+            Node* right = Tree::buildNode(preOrder, inOrder, inIndex + 1, inOrder.size() - 1, &preOrderIndex);
+
+            if (left != NULL) {
+                root->left = left;
+            }
+            if (right != NULL) {
+                root->right = right;
+            }
+
+            Tree tree = Tree();
+            tree.root = root;
+            return tree;
+        }
+
+    private:
+        static Node* buildNode(string preOrder, string inOrder, int left, int right, int* preOrderIndex) {
+            if (left <= right) {
+                (*preOrderIndex)++;
+                Node *root = new Node(preOrder[*preOrderIndex]);
+                int inIndex = indexOf(inOrder, left, right, root->value);
+                
+                Node* leftNode = Tree::buildNode(preOrder, inOrder, left, inIndex - 1, preOrderIndex);
+                Node* rightNode = Tree::buildNode(preOrder, inOrder, inIndex + 1, right, preOrderIndex);
+                
+                if (leftNode != NULL) {
+                    root->left = leftNode;
+                }
+                if (rightNode != NULL) {
+                    root->right = rightNode;
+                }
+
+                return root;
+            }
+            return NULL;
+        }
+
+        void postOrderRecursion(Node* node, string* result) {
+            if (node == NULL)
+                return;
+
+            Tree::postOrderRecursion(node->left, result);
+            Tree::postOrderRecursion(node->right, result);
+            *result += node->value;
+        }
+};
+
+int main() {
+
+    char pre[MAX_TEXT], in[MAX_TEXT];
+    while (scanf("%s", pre) != EOF && scanf("%s", in) != EOF) {
+        Tree tree = Tree::buildFromPreAndInOrder(pre, in);
+        string result = tree.postOrder();
+        printf("%s\n", result.c_str());
+    }
+
+    return 0;
+}
