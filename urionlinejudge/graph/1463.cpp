e9bf28de6a85819cc8f059b64a143f94f26ed245
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-05-13 23:50:25 -0300

    Trying the 1463 URI problem

--- urionlinejudge/graph/1463.cpp
+++ urionlinejudge/graph/1463.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string>
 #include <stack>
+#include <queue>
+#include <map>
 #include <ctype.h>
 
 #define MAX_EXPRESSION 105
@@ -28,7 +30,7 @@ int getOperatorPrecedence(char c) {
 }
 
 string toPostfix(string expression) {
-    stack<char> s;
+    stack<char> support;
     string result = "";
 
     for (int i = 0; i < expression.size(); i++) {
@@ -37,19 +39,28 @@ string toPostfix(string expression) {
 
         if (isalpha(current) || isdigit(current)) {
             result += current;
+        } else if (current == '(') {
+            support.push(current);
+        } else if (current == ')') {
+            while (true) {
+                char top = support.top();
+                support.pop();
+                if (top == '(') break;
+                result += top;
+            }
         } else if (isOperator(current)) {
             int currentPrecedence = getOperatorPrecedence(current);
-            while (!s.empty() && currentPrecedence <= getOperatorPrecedence(s.top())) {
-                result += s.top();
-                s.pop();
+            while (!support.empty() && currentPrecedence <= getOperatorPrecedence(support.top())) {
+                result += support.top();
+                support.pop();
             }
-            s.push(current);
+            support.push(current);
         }
     }
 
-    while (!s.empty()) {
-        result += s.top();
-        s.pop();
+    while (!support.empty()) {
+        result += support.top();
+        support.pop();
     }
     
     return result;
@@ -66,33 +77,75 @@ class Node {
         }
 };
 
+class NodeLevel {
+    public:
+        Node* node;
+        int level;
+
+        NodeLevel(Node* node, int level) {
+            this->node = node;
+            this->level = level;
+        }
+};
+
 class Tree {
     public:
         Node* root;
+
+        void printLevelOrder() {
+            map<int, string> levels;
+            queue<NodeLevel*> discovered;
+            discovered.push(new NodeLevel(root, 1));
+
+            while (!discovered.empty()) {
+                NodeLevel* next = discovered.front();
+
+                if (next->node->left != NULL) 
+                    discovered.push(new NodeLevel(next->node->left, next->level + 1));
+
+                if (next->node->right != NULL) 
+                    discovered.push(new NodeLevel(next->node->right, next->level + 1));
+
+                if (levels.find(next->level) != levels.end()) {
+                    levels[next->level] = next->node->value + levels[next->level];
+                } else {
+                    levels[next->level] = next->node->value;
+                }
+
+                discovered.pop();
+            }
+
+            for (auto level : levels) {
+                printf("Nivel %d: %s\n", level.first, level.second.c_str());
+            }
+            
+        }
+
+        static Tree* buildTree(string postFixExpression) {
+            stack<Node*> nodes;
+            for (int i = 0; i < postFixExpression.size(); i++) {
+                char current = postFixExpression[i];
+                if (isalpha(current) || isdigit(current)) {
+                    nodes.push(new Node(current));
+                } else {
+                    Node* pop1 = nodes.top();
+                    nodes.pop();
+                    Node* pop2 = nodes.top();
+                    nodes.pop();
+                    Node* root = new Node(current);
+                    root->left = pop1;
+                    root->right = pop2; 
+                    nodes.push(root);
+                }
+            }
+
+            Tree* tree = new Tree();
+            tree->root = nodes.top();
+            return tree; 
+        }
 };
 
-Tree* buildTree(string postFixExpression) {
-    stack<Node*> nodes;
-    for (int i = 0; i < postFixExpression.size(); i++) {
-        char current = postFixExpression[i];
-        printf("C: %c\n",current);
-        if (isalpha(current) || isdigit(current)) {
-            nodes.push(new Node(current));
-        } else {
-            Node* pop1 = nodes.top();
-            nodes.pop();
-            Node* pop2 = nodes.top();
-            nodes.pop();
-            Node* root = new Node(current);
-            root->left = pop1;
-            root->right = pop2; 
-        }
-    }
 
-    Tree* tree;
-    tree->root = nodes.top();
-    return tree; 
-}
 
 
 
@@ -100,10 +153,13 @@ int main() {
 
     char expressionC[MAX_EXPRESSION];
 
+    bool isFirstCase = true;
     while (scanf(" %[^\n]s", expressionC) != EOF) {
         string expression = expressionC;
-        printf("%s\n", toPostfix(expression).c_str());
-        Tree* tree = buildTree(toPostfix(expression));
+        Tree* tree = Tree::buildTree(toPostfix(expression));
+        if (!isFirstCase) printf("\n");
+        isFirstCase = false;
+        tree->printLevelOrder();
     }
 
 
