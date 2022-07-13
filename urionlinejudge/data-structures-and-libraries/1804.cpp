127d23bd5aacc6c08f3c57d33b1b52a8b54edf65
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-07-13 20:38:07 -0300

    Continue trying 1804 problem

--- urionlinejudge/data-structures-and-libraries/1804.cpp
+++ urionlinejudge/data-structures-and-libraries/1804.cpp
@@ -7,7 +7,7 @@
 class BITree {
 
     private:
-        int size = 0;
+        int size;
         int raw[MAX_BUGGIES];
         int indexed[MAX_BUGGIES];
 
@@ -19,14 +19,35 @@ class BITree {
         }
 
         void insert(int value) {
-            int index = this->size;
-            this->raw[index] = value;
-            while (index > 0) {
-                printf("%d\n", index);
-                this->indexed[index] += this->raw[index];
-                index -= index & (-index);
+            int initialIndex = this->size;
+            this->raw[initialIndex] = value;
+            this->indexed[initialIndex] = value;
+            int finalIndex = initialIndex - (initialIndex & (-initialIndex));
+            for (int i = initialIndex - 1; i > finalIndex; i--) {
+                this->indexed[initialIndex] += this->raw[i];
             }
             this->size++;
+        }
+
+        void remove(int at) {
+            int n = this->size;
+            int index = at + 1;
+            this->raw[index] = 0;
+            int valueBeingRemoved = this->raw[at];
+            while (index <= n) {
+                this->indexed[index] -= valueBeingRemoved;
+                index += index & (-index);
+            }
+        }
+
+        int sum(int untilIndex) {
+            int result = 0;
+            int index = untilIndex + 1;
+            while (index > 0) {
+                result += this->indexed[index];
+                index -= index & (-index);
+            }
+            return result;
         }
 
         void print() {
@@ -40,12 +61,27 @@ class BITree {
 };
 
 int main() {
+    
+    int n; scanf("%d", &n);
     BITree* tree = new BITree();
-    tree->insert(1);
-    tree->insert(2);
-    tree->insert(3);
-    tree->print();
-    // printf("oi\n");
+    while (n--) {
+        printf("n: %d\n", n);
+        int x; scanf("%d", &x);
+        tree->insert(x);
+    }
+
+    char s[100];
+    int operation = 0, value = 0;
+    scanf(" %[^\n]", s);
+    printf("S: %s\n", s);
+    // while (scanf("%d %d", &operation, &value) != EOF) {
+        // if (operation == 'a')
+        printf(">> %d %d\n", operation, value);
+        // for (int i = 0; i < 100000000; i++) {}
+            // tree->remove(value - 1);
+        // else
+            // printf("%d\n", tree->sum(value - 1));
+    // }
 
     return 0;
 }
