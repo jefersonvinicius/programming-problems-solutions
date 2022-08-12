91d4e6ee3ce36cfa7a128b585d20a9c56bf0793a
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-08-12 20:19:03 -0300

    Finished URI 2539 problem

--- urionlinejudge/data-structures-and-libraries/2539.cpp
+++ urionlinejudge/data-structures-and-libraries/2539.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 #define MAX_PLAYERS 1000000
+#define ull unsigned long long
 
 class BITree {
     public:
@@ -26,25 +27,24 @@ class BITree {
             int result = 0;
             for (int i = startAt; i < this->size; i++) {
                 result += this->raw[i];
-                // result += 1;
             }
             this->indexed[this->size] = result;
             this->size++;
         }
 
         void remove(int index) {
-            int value = -this->raw[index];
-            printf("Value: %d\n", value);
-            for (int i = index + (index & (-index)); i <= this->size; i += i & (-i)) {
-                this->indexed[i] += value;
+            int value = this->raw[index];
+            int correctIndex = this->playerAmount - index;
+            for (int i = correctIndex; i <= this->size; i += i & (-i)) {
+                this->indexed[i] -= value;
             }
             this->raw[index] = 0;
         }
 
-        int sum(int start) {
-            int result = 0;
-            for (int i = start; i > 0; i -= i & (-i)) {
-                // printf("index: %d\n", i);
+        ull sum(int start) {
+            ull result = 0;
+            int correctIndex = this->playerAmount - start;
+            for (int i = correctIndex; i > 0; i -= i & (-i)) {
                 result += this->indexed[i];
             }
             return result;
@@ -59,63 +59,29 @@ void print_array(int* array, size_t size, int start = 1) {
 }
 
 int main() {
-
-
     int n;
-     
     while (scanf("%d", &n) != EOF) {
-        int players[n + 1], playersIndexes[n + 1];
+        int playersIndexes[n + 1];
         
         BITree* tree = new BITree(n + 1);
 
         for (int i = 1; i <= n; i++) {
             int x; scanf("%d", &x);
-            players[i] = x;
             playersIndexes[x] = i;
         }
 
-        for (int i = n-1; i >= 0; i--)
+        for (int i = 0; i < n; i++)
             tree->insert(1);
 
-        int memo[n + 1];
-        for (int i = 1; i <= n; i++) {
-            int player = players[i];
-            int playerIndex = playersIndexes[player];
-            memo[player] = n - playerIndex;
+        ull result = 0;
+        for (int player = 1; player <= n; player++) {
+            int index = playersIndexes[player];
+            result += tree->sum(index);
+            tree->remove(index);
         }
+        printf("%lld\n", result);
 
-        printf("raw: ");
-        print_array(tree->raw, tree->playerAmount);
-        printf("indexed: ");
-        print_array(tree->indexed, tree->playerAmount);
-        // printf("memo: ");
-        // print_array(memo, n+1);
-
-
-        // int result = 0;
-        // for (int player = 1; player <= n; player++) {
-        //     result += memo[player];
-        //     int playerIndex = playersIndexes[player];
-        //     for (int i = playerIndex - 1; i >= 0; i--) {
-        //         int playerToUpdate = playersIndexes[i];
-        //         memo[playerToUpdate]--;
-        //     }
-        // }
-        // printf("%d\n", result);
-        printf("result: 1 -> %d\n", tree->sum(1));
-        printf("result: 2 -> %d\n", tree->sum(2));
-        printf("result: 3 -> %d\n", tree->sum(3));
-        printf("result: 4 -> %d\n", tree->sum(4));
-        
-        tree->remove(3);
-        printf("raw: ");
-        print_array(tree->raw, tree->playerAmount);
-        printf("indexed: ");
-        print_array(tree->indexed, tree->playerAmount);
-        printf("result: 1 -> %d\n", tree->sum(1));
-        printf("result: 2 -> %d\n", tree->sum(2));
-        printf("result: 3 -> %d\n", tree->sum(3));
-        printf("result: 4 -> %d\n", tree->sum(4));
+        free(tree);
     }
 
     return 0;
