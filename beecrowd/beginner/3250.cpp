8f5b80a5495017083d1a07a1e7dd77cea1690af8
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-09-30 20:37:53 -0300

    Trying beecrowd 3250 problem

---
+++ beecrowd/beginner/3250.cpp
@@ -0,0 +1,37 @@
+#include "stdio.h"
+#include "string.h"
+
+#define MAX_FLOORS 1000000
+
+int main() {
+
+    int floors, start, goal, up, down; scanf("%d%d%d%d%d", &floors, &start, &goal, &up, &down);
+
+    int press = 0, current = start;
+    int visited[MAX_FLOORS]; memset(visited, 0, MAX_FLOORS);
+    while (true) {
+        if (visited[current] == 1) {
+            press = -1;
+            break;
+        }
+
+        if (current == goal) break;
+
+        visited[current] = 1;
+        if (current < goal && current + up <= floors) {
+            current += up;
+            press++;
+        } else if (current - down >= 0) {
+            current -= down;
+            press++;
+        }
+    }   
+
+    if (press == -1) {
+        printf("use the stairs\n");
+    } else {
+        printf("%d\n", press);
+    }
+
+    return 0;
+}
