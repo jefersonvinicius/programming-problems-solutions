388c8eb6e80a831d7d811e017456294fbfd994c5
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2023-02-19 12:28:47 -0300

    Add HR Kangaroo problem solution

---
+++ hackerrank/implementation/kangaroo.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+const hasDecimal = (value) => value % 1 > 0;
+
+/*
+ * Complete the 'kangaroo' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ * 
+ * 
+ *  Reference: https://mundoeducacao.uol.com.br/fisica/encontro-ultrapassagem-uma-aplicacao-movimento-uniforme-.htm
+ */
+function kangaroo(x1, v1, x2, v2) {
+    if (v2 >= v1) return 'NO'
+    const result = (x2 - x1) / (v1 - v2)
+    return result >= 0 && !hasDecimal(result) ? 'YES' : 'NO'
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+    const x1 = parseInt(firstMultipleInput[0], 10);
+    const v1 = parseInt(firstMultipleInput[1], 10);
+    const x2 = parseInt(firstMultipleInput[2], 10);
+    const v2 = parseInt(firstMultipleInput[3], 10);
+    const result = kangaroo(x1, v1, x2, v2);
+    ws.write(result + '\n');
+    ws.end();
+}
